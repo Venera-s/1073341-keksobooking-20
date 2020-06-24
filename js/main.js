18e05d873de9df2c25c3692742aacb5c2b5700ca
Venera <coldlives@gmail.com>
2020-06-24 21:31:38 +0300

    Исправила замечания наставника

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var ADVERTS_AMOUNT = 8;
-var PLACE_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var ROOMS_AMOUNT_MAX = 100;
 var GUESTS_AMOUNT_MAX = 10;
 var LOCATION_Y_MIN = 130;
@@ -12,20 +11,41 @@ var MAP_PIN_ROUND_HALF_HEIGHT = 31;
 var TITLE_LENGTH_MIN = 30;
 var TITLE_LENGTH_MAX = 100;
 var PRICE_MAX = 1000000;
-var CHEKINS = ['12:00', '13:00', '14:00'];
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
+var MOUSE_DOWN_LEFT = 0;
+var KEY_CODE_ENTER = 'Enter';
+
+var PLACE_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var CHEKINS = ['12:00', '13:00', '14:00'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
-var KEY_CODE_ENTER = 13;
-var MOUSE_DOWN_LEFT = 0;
+
+var PRICES_FOR_TYPES = {
+  'bungalo': 0,
+  'flat': 1000,
+  'house': 5000,
+  'palace': 10000
+};
+
 var map = document.querySelector('.map');
 var mapPins = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var mapPin = document.querySelector('.map__pin--main');
+var adForm = document.querySelector('.ad-form');
+var formElements = document.querySelectorAll('form input, form select, form textarea, .ad-form__submit');
+var fieldAddress = adForm.querySelector('#address');
+var titleField = adForm.querySelector('#title');
+var housePrice = adForm.querySelector('#price');
+var typeHouse = adForm.querySelector('#type');
+var roomNumber = adForm.querySelector('#room_number');
+var capacity = adForm.querySelector('#capacity');
+var timeIn = adForm.querySelector('#timein');
+var timeOut = adForm.querySelector('#timeout');
 
 var getRandomInt = function (min, max) {
   min = Math.ceil(min);
@@ -103,37 +123,27 @@ var getMapPins = function (announcements) {
   return fragment;
 };
 
-var formElements = document.querySelectorAll('form input, form select, form textarea, .ad-form__submit');
-
-var disabledForm = function (elements) {
+var disableForm = function (elements) {
   for (var i = 0; i < elements.length; i++) {
     elements[i].toggleAttribute('disabled');
   }
 };
 
-disabledForm(formElements);
-
-var mapPin = document.querySelector('.map__pin--main');
-var adForm = document.querySelector('.ad-form');
-var fieldAddress = adForm.querySelector('#address');
-
 // Заполнение поля адреса
 
-var pin = function (mapPinHeight) {
+var setAddress = function (mapPinHeight) {
   var coordinateX = Math.round(mapPin.offsetLeft + MAP_PIN_WIDTH / 2);
   var coordinateY = mapPin.offsetTop + Math.round(mapPinHeight);
   fieldAddress.value = coordinateX + ', ' + coordinateY;
 };
 
-pin(MAP_PIN_ROUND_HALF_HEIGHT);
-
 // Активация страницы
 
-var activePage = function () {
-  pin(MAP_PIN_HEIGHT);
+var activatePage = function () {
+  setAddress(MAP_PIN_HEIGHT);
   map.classList.remove('map--faded');
   adForm.classList.remove('ad-form--disabled');
-  disabledForm(formElements);
+  disableForm(formElements);
   var allAnnouncements = getAnnouncements(ADVERTS_AMOUNT);
   mapPins.appendChild(getMapPins(allAnnouncements));
   mapPin.removeEventListener('mousedown', onMapPinClick);
@@ -142,73 +152,82 @@ var activePage = function () {
 
 var onMapPinClick = function (evt) {
   if (evt.button === MOUSE_DOWN_LEFT) {
-    activePage();
+    activatePage();
   }
 };
 
 var onMapPinDown = function (evt) {
   if (evt.keyCode === KEY_CODE_ENTER) {
-    activePage();
+    activatePage();
   }
 };
+
+// Валидация формы
+
+titleField.addEventListener('input', function () {
+  var titleFieldLength = titleField.value.length;
+  var validationResult = '';
+
+  if (titleField.validity.valueMissing) {
+    validationResult = 'Заполните пожалуйста поле';
+  } else if (titleFieldLength < TITLE_LENGTH_MIN) {
+    validationResult = 'Еще нужно ввести ' + (TITLE_LENGTH_MIN - titleFieldLength) + ' символов';
+  } else if (titleFieldLength > TITLE_LENGTH_MAX) {
+    validationResult = 'Удалите лишние ' + (TITLE_LENGTH_MAX - titleFieldLength) + ' символов';
+  }
+
+  titleField.reportValidity();
+  titleField.setCustomValidity(validationResult);
+});
+
+var onPriceInput = function () {
+  housePrice.reportValidity();
+};
+
+var onTypeChange = function () {
+  housePrice.setAttribute('placeholder', PRICES_FOR_TYPES[typeHouse.value]);
+  housePrice.setAttribute('min', PRICES_FOR_TYPES[typeHouse.value]);
+  housePrice.reportValidity();
+};
+
+var onRoomsGuestsInput = function () {
+  var rooms = parseInt(roomNumber.value, 10);
+  var guests = parseInt(capacity.value, 10);
+  var roomValue = roomNumber.selectedIndex;
+  var capacityValue = capacity.selectedIndex;
+  var validationResult = '';
+  if (rooms === ROOMS_AMOUNT_MAX && guests !== 0) {
+    validationResult = 'Для ' + roomNumber.options[roomValue].label + ' предназначено ' + capacity.options[roomValue].label;
+  } else if (guests === 0 && rooms !== ROOMS_AMOUNT_MAX) {
+    validationResult = 'Для ' + capacity.options[capacityValue].label + ' предназначено ' + roomNumber.options[capacityValue].label;
+  } else if (rooms < guests) {
+    validationResult = 'Количество комнат не должно превышать количество гостей';
+  }
+
+  roomNumber.reportValidity();
+  roomNumber.setCustomValidity(validationResult);
+};
+
+var onTimeChange = function (evt) {
+  var value = evt.target.value;
+  timeIn.value = value;
+  timeOut.value = value;
+};
+
+disableForm(formElements);
+
+setAddress(MAP_PIN_ROUND_HALF_HEIGHT);
 
 mapPin.addEventListener('mousedown', onMapPinClick);
 mapPin.addEventListener('keydown', onMapPinDown);
 
-// Валидация формы
+typeHouse.addEventListener('change', onTypeChange);
+housePrice.addEventListener('input', onPriceInput);
 
-var titleField = adForm.querySelector('#title');
+onRoomsGuestsInput();
 
-titleField.addEventListener('input', function () {
-  var titleFieldLength = titleField.value.length;
+roomNumber.addEventListener('input', onRoomsGuestsInput);
+capacity.addEventListener('input', onRoomsGuestsInput);
 
-  if (titleField.validity.valueMissing) {
-    titleField.setCustomValidity('Заполните пожалуйста поле');
-  } else if (titleFieldLength < TITLE_LENGTH_MIN) {
-    titleField.setCustomValidity('Еще нужно ввести ' + (TITLE_LENGTH_MIN - titleFieldLength) + ' символов');
-
-  } else if (titleFieldLength > TITLE_LENGTH_MAX) {
-    titleField.setCustomValidity('Удалите лишние ' + (TITLE_LENGTH_MAX - titleFieldLength) + ' символов');
-
-  } else {
-    titleField.setCustomValidity('');
-  }
-});
-
-var housePrice = adForm.querySelector('#price');
-var typeHouse = adForm.querySelector('#type');
-
-var pricesForTypes = {
-  'bungalo': 0,
-  'flat': 1000,
-  'house': 5000,
-  'palace': 10000
-};
-
-typeHouse.addEventListener('input', function () {
-  housePrice.setAttribute('placeholder', pricesForTypes[typeHouse.value]);
-  housePrice.setAttribute('min', pricesForTypes[typeHouse.value]);
-});
-
-var roomNumber = adForm.querySelector('#room_number');
-var capacity = adForm.querySelector('#capacity');
-
-var roomGuests = function () {
-  var roomValue = roomNumber.selectedIndex;
-  var capacityValue = capacity.selectedIndex;
-  if (roomNumber.value === '100' && capacity.value !== '0') {
-    roomNumber.setCustomValidity('Для ' + roomNumber.options[roomValue].label + ' предназначено ' + capacity.options[roomValue].label);
-  } else if (roomNumber.value < capacity.value) {
-    roomNumber.setCustomValidity('Количество комнат не должно превышать количество гостей');
-  } else if (capacity.value === '0' && roomNumber.value !== '100') {
-    roomNumber.setCustomValidity('Для ' + capacity.options[capacityValue].label + ' предназначено ' + roomNumber.options[capacityValue].label);
-  } else {
-    roomNumber.setCustomValidity('');
-  }
-};
-
-roomGuests();
-
-roomNumber.addEventListener('input', roomGuests);
-capacity.addEventListener('input', roomGuests);
-
+timeIn.addEventListener('change', onTimeChange);
+timeOut.addEventListener('change', onTimeChange);
